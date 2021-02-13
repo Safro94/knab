89634811b49c56ffd537af17e4323e1d35f9eea1
Matias Safranchik <Mati@MacBook-Pro-de-Matias.local>
2021-02-13 17:00:46 -0300

    add fetcher function

---
+++ server/src/utils/__tests__/fetch.test.js
@@ -0,0 +1,73 @@
+jest.dontMock('../fetch');
+
+const fetch = require('../fetch');
+const nodeFetch = require('node-fetch')
+
+jest.mock('node-fetch');
+
+describe.each([
+    ['http://localhost:port', { test }],
+    ['http://localhost:port', { 'Authorization': 'Bearer abc' }],
+    ['http://localhost:port', null, 'post']
+])('Fetch arguments',(url, headers, method) => {
+    it('should call fetch with the right arguments', async() => {
+        //Act
+        await fetch({ url, headers, method });
+
+        //Assert
+        expect(nodeFetch).toHaveBeenCalledTimes(1);
+        expect(nodeFetch).toHaveBeenCalledWith(url, {
+            method: method || 'get',
+            headers: {
+                ...headers || '',
+                'Content-Type': 'application/json'
+            },
+        });
+    });
+});
+
+describe('Fetch', () => {
+    const params = { url: 'url' };
+    const fetchResult = { test }
+
+    it('should return a json', async() => {
+        //Arrange
+        nodeFetch.mockImplementation(() => {
+            return {
+                json: () => ({ test })
+            }
+        });
+
+        //Act
+        const result = await fetch(params);
+
+        //Assert
+        expect(result).toEqual(fetchResult);
+    });
+
+    it('should return undefined', async() => {
+        //Arrange
+        nodeFetch.mockImplementation(() => null);
+
+        //Act
+        const result = await fetch(params);
+
+        //Assert
+        expect(result).not.toBeDefined();
+    });
+
+    it('should return an empty object', async() => {
+        //Arrange
+        nodeFetch.mockImplementation(() => {
+            return {
+                json: () => null
+            }
+        });
+
+        //Act
+        const result = await fetch(params);
+
+        //Assert
+        expect(result).toEqual({});
+    });
+});
