cbf961bd8ce19c55ab03c7a398b4520a9453e2f8
Matias Safranchik <Mati@MacBook-Pro-de-Matias.local>
2021-02-13 16:58:53 -0300

    add error handler middleware

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -6,6 +6,8 @@ const cors = require('cors');
 const rateLimit = require('express-rate-limit');
 const slowDown = require('express-slow-down');
 
+const errorHandler = require('../middlewares/errorHandler');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -32,6 +34,7 @@ app.use(express.json());
 // Not found
 
 // Error handler
+app.use(errorHandler);
 
 // eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Server running on port ${port}`));
