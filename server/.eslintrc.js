639092e10f7519e0354a7ed01743c3053db4996c
Matias Safranchik <Mati@MacBook-Pro-de-Matias.local>
2021-02-13 16:55:28 -0300

    setup project

---
+++ server/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+	env: {
+		browser: true,
+		commonjs: true,
+		es2021: true,
+	},
+	extends: ['airbnb-base', 'plugin:jest/recommended'],
+	parserOptions: {
+		ecmaVersion: 12,
+	},
+	rules: {
+		'linebreak-style': 0,
+		'arrow-parens': ['error', 'as-needed'],
+		'consistent-return': 0,
+		'implicit-arrow-linebreak': 0,
+		indent: ['error', 2],
+		'class-methods-use-this': 0,
+	},
+};
---
+++ server/src/api/index.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const express = require('express');
+const compression = require('compression');
+const helmet = require('helmet');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+const slowDown = require('express-slow-down');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+const limiter = rateLimit({
+	windowMs: 60 * 1000,
+	max: 10,
+});
+
+const speedLimiter = slowDown({
+	windowMs: 60 * 1000,
+	delayAfter: 10,
+	delayMs: 500,
+});
+
+app.use(limiter);
+app.use(speedLimiter);
+app.use(compression());
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+// Routes
+
+// Not found
+
+// Error handler
+
+// eslint-disable-next-line no-console
+app.listen(port, () => console.log(`Server running on port ${port}`));
